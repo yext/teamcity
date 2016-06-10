30aabea77b0ae18b69e9cbb272973f8e44cab683
Namrata Kodali <nkodali@yext.com>
2016-06-10 14:14:32 -0400

    yext/teamcity: use TeamCityDate instead of string for date types R=robfig

--- build.go
+++ build.go
@@ -1,5 +1,14 @@
 package teamcity
 
+import (
+	"strings"
+	"time"
+)
+
+const (
+	dateFormat = "20060102T150405-0700"
+)
+
 // Builds is a list of builds
 type Builds struct {
 	Builds []Build `json:"build"`
@@ -15,9 +24,9 @@ type Build struct {
 	State       string      `json:"state"`
 	Href        string      `json:"href"`
 	StatusText  string      `json:"statusText"`
-	QueuedDate  string      `json:"queuedDate"`
-	StartDate   string      `json:"startDate"`
-	FinishDate  string      `json:"finishDate"`
+	QueuedDate  Time        `json:"queuedDate"`
+	StartDate   Time        `json:"startDate"`
+	FinishDate  Time        `json:"finishDate"`
 	LastChanges LastChanges `json:"lastChanges"`
 }
 
@@ -36,4 +45,22 @@ type Change struct {
 	Id       int    `json:"id"`
 	Version  string `json:"version"`
 	Username string `json:"username"`
+}
+
+// Time is the date in the format TeamCity provides
+type Time time.Time
+
+// UnmarshalJSON unmarshals the time using the TeamCity format
+func (t *Time) UnmarshalJSON(buf []byte) error {
+	tt, err := time.Parse(dateFormat, strings.Trim(string(buf), `"`))
+	if err != nil {
+		return err
+	}
+	*t = Time(tt)
+	return nil
+}
+
+// MarshalJSON marshals the time using the TeamCity format
+func (t Time) MarshalJSON() ([]byte, error) {
+	return []byte(`"` + time.Time(t).Format(dateFormat) + `"`), nil
 }
