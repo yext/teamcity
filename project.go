0da293323678f79c2df77787ea25cc065a39e9fe
Nathan Williams <nwilliams@yext.com>
2016-06-10 15:44:27 -0400

    teamcity: store project parameters from teamcity json response r=vple

--- project.go
+++ project.go
@@ -4,9 +4,22 @@ package teamcity
 type Project struct {
 	Name   string `json:"name"`
 	WebUrl string `json:"webUrl"`
+	Params Params `json:"parameters"`
 }
 
 // Projects is a list of TeamCity projects and aggregate details
 type Projects struct {
 	Projects []Project `json:"project"`
+}
+
+// Params is a container for the various properties of a project
+type Params struct {
+	Properties []Property `json:"property"`
+}
+
+// Property is a characteristic of a project (e.g. JOB, OWNER, or SERVICE)
+type Property struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+	Own   bool   `json:"own"`
 }
